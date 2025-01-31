368773810c03496bdf677bfeb7abf5689c0bd908
charliechamberlayne <113937012+charliechamberlayne@users.noreply.github.com>
2025-01-31 18:10:58 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,7 @@ st.title('Machine Learning App 🤖')
 
 st.info('This app builds a machine learning model')
 
-df = pd.read.csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-df
+with st.expander('Data'):
+  st.write('**Raw data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
